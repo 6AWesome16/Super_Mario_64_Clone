d7173851b8146a692f3ff06da609f2aaf05fb701
Noelle <noelledino@gmail.com>
2017-11-08 11:38:52 -0500

    Basic Pickup Script Added
    temp coin sprite also created for the sake of my sanity bc a bunch of floating mario heads = uncomfortable

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public int CoinCount;
+	public Text CoinsText;
+
+	void Start () {
+
+		Time.timeScale = 1;
+	}
+
+	void Update () {
+
+		CoinsText.text = ("" + CoinCount);
+	}
+}
---
+++ Assets/Scripts/MarioPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioPickup : MonoBehaviour {
+
+	private GameManager gm;
+
+	void Start () {
+		gm = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameManager> ();
+	}
+
+	//The Coins must be tagged as YellowCoin in the inspector
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.CompareTag ("YellowCoin")) {
+			Destroy (col.gameObject);
+			gm.CoinCount += 1;
+		}
+	}
+}
