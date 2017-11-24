dee65293aec6425541d012882bd1c07d50ce6b25
4m4nd4 <jackchiarello@gmail.com>
2017-11-24 16:13:47 -0500

    Goomba movement stuff (With Bug!!)

---
+++ Assets/Scripts/GoombaMarioDiscover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoombaMarioDiscover : MonoBehaviour {
+
+	public Transform mario; // set in inspector please
+
+
+	void OnTriggerEnter(Collider collision){
+
+		// if i touch mario, my goomba parent sees him
+
+		if (collision.transform == mario) {
+			transform.parent.GetComponent<GoombaMovement> ().marioSighted = true;
+		}
+
+	}
+
+}
